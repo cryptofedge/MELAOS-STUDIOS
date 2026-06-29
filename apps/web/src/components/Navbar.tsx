e2ac8ba213cad090f9f49a972661c9ca5220451c
eclatstreamers <cryptofedge@gmail.com>
2026-06-29 12:36:33 -0400

    fix: increase navbar logo size from h-10 to h-16

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-4 h-16 flex items-center justify-between gap-4">
           {/* Logo */}
           <Link href="/" className="shrink-0">
-            <img src="/melaos-logo-2.png" alt="MELAOS STUDIOS" className="h-10 w-auto" />
+            <img src="/melaos-logo-2.png" alt="MELAOS STUDIOS" className="h-16 w-auto" />
           </Link>
 
           {/* Desktop search */}
